fc2c8bbb944fcb6cfd2cfaf1ca0608eec0e05066
Kevin Johnson <kevin@coolkev.com>
2015-02-17 23:11:15 -0500

    Context Menu is now enabled for the cues list

--- src/syncedplay/SyncedPlay.java
+++ src/syncedplay/SyncedPlay.java
@@ -3,7 +3,12 @@ package syncedplay;
 import java.awt.Dimension;
 import java.awt.KeyEventDispatcher;
 import java.awt.KeyboardFocusManager;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.File;
 import java.util.ArrayList;
 import javax.sound.sampled.UnsupportedAudioFileException;
@@ -15,6 +20,7 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
@@ -25,8 +31,14 @@ import javax.swing.UnsupportedLookAndFeelException;
 
 public class SyncedPlay extends JFrame {
 
-    SoundManager sm;
-    CueTableModel cueTableModel;
+    private SoundManager sm;
+    private CueTableModel cueTableModel;
+    
+    private Point contextClickPoint;
+    private JTextField commandPromptText;
+    private JTable cuesTable;
+    private JPopupMenu tablePMenu;
+
     
     public SyncedPlay() {
         this.cueTableModel = new CueTableModel();
@@ -81,6 +93,24 @@ public class SyncedPlay extends JFrame {
         menubar.add(file);
         setJMenuBar(menubar);
     }
+    
+    final void buildPopUpMenu(){
+        tablePMenu = new JPopupMenu();
+        
+        JMenuItem editCue = new JMenuItem("Edit Cue");
+        editCue.addActionListener(new ActionListener() {
+            
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Point pt = ((JPopupMenu) ((JMenuItem) e.getSource()).getParent()).getLocation(); //TODO: This is supposed to get the screen location, of the upper right corner of the popup menu, but this doesn't work.
+                int row = cuesTable.rowAtPoint(contextClickPoint);
+                //System.out.println(pt);
+                System.out.println("Editing: " + cueTableModel.getCue(row).getDescription());
+            }
+        });
+
+        tablePMenu.add(editCue);
+    }
 
     final void makePanel() {
         JPanel basic = new JPanel();
@@ -96,19 +126,35 @@ public class SyncedPlay extends JFrame {
         cues.add(new Cue("Two"));
         cues.add(new Cue("Three"));
         cueTableModel.setCues(cues);
-        JTable cuesTable = new JTable(cueTableModel);
+        cuesTable = new JTable(cueTableModel);
         cuesTable.getColumnModel().getColumn(0).setMaxWidth(35);
 
         cuesPane.getViewport().add(cuesTable);
         basic.add(cuesPane);
 
-        JTextField commandPromptText = new JTextField(20);
-        commandPromptText.setText("kevin@kevin-mint-devel ~ $");
+        commandPromptText = new JTextField(20);
+        commandPromptText.setText("");
         basic.add(commandPromptText);
         basic.add(Box.createRigidArea(new Dimension(0, 15)));
     }
     
     final void setUpKeyCaptures(){
+        cuesTable.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent me) {
+                JTable table =(JTable) me.getSource();
+                Point p = me.getPoint();
+                int row = table.rowAtPoint(p);
+                if (me.getClickCount() == 2) {
+                    String description = cueTableModel.getCue(row).getDescription();
+                    System.out.println(description);
+                }
+                if (me.getButton() == MouseEvent.BUTTON3) {
+                    tablePMenu.show(me.getComponent(), me.getX(), me.getY());
+                    contextClickPoint = p;
+                }
+            }
+        });
         KeyEventDispatcher keyEventDispatcher = new KeyEventDispatcher() {
             @Override
             public boolean dispatchKeyEvent(final KeyEvent e) {
@@ -122,7 +168,7 @@ public class SyncedPlay extends JFrame {
                         return true;
                     } else if (e.getKeyChar() == '\n') {
                         System.out.println("Command Run");
-                        cueTableModel.addCue(new Cue("Kevin"));
+                        cueTableModel.addCue(new Cue(commandPromptText.getText()));
                         /*try {
                             sm.playSound("knock");
                         } catch (UnsupportedAudioFileException ex) {
@@ -152,6 +198,8 @@ public class SyncedPlay extends JFrame {
         makePanel();
         
         setUpKeyCaptures();
+        
+        buildPopUpMenu();
         
         setTitle("Synced Play");
         setSize(400, 400);
