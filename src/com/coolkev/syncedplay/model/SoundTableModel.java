c24a9e79e3a3428e4d5057e57075405c5b326e64
Kevin Johnson <kevin@coolkev.com>
2015-02-22 13:42:19 -0500

    Stopped working with threads, and now I'm working with Runnables in the SoundTableModel.

--- src/com/coolkev/syncedplay/model/SoundTableModel.java
+++ src/com/coolkev/syncedplay/model/SoundTableModel.java
@@ -28,72 +28,149 @@ import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.table.AbstractTableModel;
 
+class AudioListener implements LineListener {
+    public boolean done = false;
+    public boolean isKilled = false;
+    
+            @Override
+            public synchronized void update(LineEvent event) {
+                LineEvent.Type eventType = event.getType();
+                if (eventType == LineEvent.Type.STOP || eventType == LineEvent.Type.CLOSE) {
+                    done = true;
+                    notifyAll();
+                }
+            }
+
+            public synchronized void waitUntilDone() throws InterruptedException {
+                while (!done) {
+                    if (isKilled) {
+                        throw new InterruptedException();
+                    } else {
+                        wait();
+                    }
+                }
+            }
+        }
+
 /**
  *
  * @author kevin
  */
-public class SoundTableModel extends AbstractTableModel {
-    
-    TreeMap<String, File> keyToFile = new TreeMap();
-    
-    TreeMap<String, ArrayList<Thread>> keyToThreads = new TreeMap();
+class SoundRunnable implements Runnable {
 
-    public SoundTableModel(){
+    private boolean isKilled = false;
+    private final boolean loop;
+    private Clip clip;
+    final public AudioListener listener;
+    // The wrapper thread is unnecessary, unless it blocks on the
+    // Clip finishing; see comments.
+    File clipFile;
+
+    public SoundRunnable(File cf, boolean loop) {
+        this.clipFile = cf;
+        this.loop = loop;
+        this.listener = new AudioListener();
+    }
+
+    public void kill() {
+        isKilled = true;
+        listener.done = true;
+        synchronized (listener){
+            listener.notifyAll();
+        }
+    }
+
+    public boolean isAlive() {
+        return !(isKilled || listener.done);
+    }
+
+    @Override
+    public void run() {
+        try (AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(clipFile)) {
+            clip = AudioSystem.getClip();
+            clip.addLineListener(listener);
+            clip.open(audioInputStream);
+            try {
+                if (loop) {
+                    clip.loop(Clip.LOOP_CONTINUOUSLY);
+                } else {
+                    clip.start();
+                }
+                listener.waitUntilDone();
+            } catch (InterruptedException ex) {
+                //Ok, we're being stopped;
+            } finally {
+                clip.close();
+            }
+        } catch (LineUnavailableException | UnsupportedAudioFileException | IOException ex) {
+            Logger.getLogger(SoundTableModel.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
+
+public class SoundTableModel extends AbstractTableModel {
+
+    TreeMap<String, File> keyToFile = new TreeMap();
+
+    TreeMap<String, ArrayList<SoundRunnable>> keyToRunnable = new TreeMap();
+
+    public SoundTableModel() {
         super();
     }
-    
+
     public void learnSound(String key, File file) {
         keyToFile.put(key, file);
-        keyToThreads.put(key, new ArrayList<Thread>());
+        keyToRunnable.put(key, new ArrayList<SoundRunnable>());
         fireTableDataChanged();
     }
-    
-    public void blank(){
+
+    public void blank() {
         panic();
         cleanUpDeadThreads();
         keyToFile = new TreeMap();
-        keyToThreads = new TreeMap();
+        keyToRunnable = new TreeMap();
         fireTableDataChanged();
     }
-    
-    public void load(String s, String dir){
+
+    public void load(String s, String dir) {
         Map<String, String> parsedString = IniFormatParser.parseIniFormat(s);
-        for (String key : parsedString.keySet()){
+        for (String key : parsedString.keySet()) {
             learnSound(key, new File(dir + "/" + parsedString.get(key)));
         }
     }
-    
-    public void runAction(Action a){
-        if (a instanceof PlaySoundAction){
+
+    public void runAction(Action a) {
+        if (a instanceof PlaySoundAction) {
             PlaySoundAction psa = (PlaySoundAction) a;
-            if (keyToFile.keySet().contains(psa.getKeyword())){
+            if (keyToFile.keySet().contains(psa.getKeyword())) {
                 playSound(psa.getKeyword());
             }
-        } else if (a instanceof StopSoundAction){
+        } else if (a instanceof StopSoundAction) {
             StopSoundAction ssa = (StopSoundAction) a;
-            if (keyToFile.keySet().contains(ssa.getKeyword())){
+            if (keyToFile.keySet().contains(ssa.getKeyword())) {
                 stopSound(ssa.getKeyword());
             }
-        } else if (a instanceof LoopSoundAction){
+        } else if (a instanceof LoopSoundAction) {
             LoopSoundAction lsa = (LoopSoundAction) a;
-            if (keyToFile.keySet().contains(lsa.getKeyword())){
+            if (keyToFile.keySet().contains(lsa.getKeyword())) {
                 loopSound(lsa.getKeyword());
             }
-        } else if (a instanceof PanicSoundAction){
+        } else if (a instanceof PanicSoundAction) {
             panic();
         }
     }
-    
+
     public String save() {
         StringBuilder out = new StringBuilder();
-        for (String key : keyToFile.keySet()){
+        for (String key : keyToFile.keySet()) {
             out.append("[").append(key).append("]\n");
             out.append(keyToFile.get(key).getName()).append("\n");
         }
         return out.toString();
     }
 
-    public static boolean isFileSupported(File f){
+    public static boolean isFileSupported(File f) {
         try {
             AudioSystem.getAudioInputStream(f);
             return true;
@@ -101,142 +178,102 @@ public class SoundTableModel extends AbstractTableModel {
             return false;
         }
     }
-    
-    public String keyFromRow(int row){
+
+    public String keyFromRow(int row) {
         return (String) getValueAt(row, 0);
     }
-    
-    public void stopSound(final String key){
-        cleanUpDeadThreads();
-        ArrayList<Thread> threadList = keyToThreads.get(key);
-        for (Thread thread : threadList){
-            thread.interrupt();
+
+    public void stopSound(final String key) {
+        ArrayList<SoundRunnable> runnableList = keyToRunnable.get(key);
+        for (SoundRunnable r : runnableList) {
+            r.kill();
         }
-        /*    for (Iterator<Thread> iterator = threadList.iterator(); iterator.hasNext();) {
-                Thread thread = iterator.next();
-                if (!thread.isAlive()) {
-                    iterator.remove();
-                }
-            }*/
+        cleanUpDeadThreads();
     }
-    
-    public void panic(){
-        Set<String> keys = keyToThreads.keySet();
-        for (String key : keys){
-            ArrayList<Thread> threadList = keyToThreads.get(key);
-            for (Iterator<Thread> iterator = threadList.iterator(); iterator.hasNext();) {
-                Thread thread = iterator.next();
-                if (thread.isAlive()) {
-                    thread.interrupt();
+
+    public void panic() {
+        Set<String> keys = keyToRunnable.keySet();
+        for (String key : keys) {
+            ArrayList<SoundRunnable> runnableList = keyToRunnable.get(key);
+            for (Iterator<SoundRunnable> iterator = runnableList.iterator(); iterator.hasNext();) {
+                SoundRunnable soundRunnable = iterator.next();
+                if (soundRunnable.isAlive()) {
+                    soundRunnable.kill();
                 }
                 iterator.remove();
             }
         }
     }
-    
-    public File[] getFiles(){
+
+    public File[] getFiles() {
         return keyToFile.values().toArray(new File[keyToFile.values().size()]);
     }
-    
-    private void cleanUpDeadThreads(){
-        Set<String> keys = keyToThreads.keySet();
-        for (String key : keys){
-            ArrayList<Thread> threadList = keyToThreads.get(key);
-            for (Iterator<Thread> iterator = threadList.iterator(); iterator.hasNext();) {
-                Thread thread = iterator.next();
-                if (!thread.isAlive()) {
+
+    private void cleanUpDeadThreads() {
+        Set<String> keys = keyToRunnable.keySet();
+        for (String key : keys) {
+            ArrayList<SoundRunnable> runnableList = keyToRunnable.get(key);
+            for (Iterator<SoundRunnable> iterator = runnableList.iterator(); iterator.hasNext();) {
+                SoundRunnable soundRunnable = iterator.next();
+                if (!soundRunnable.isAlive()) {
                     iterator.remove();
                 }
             }
         }
     }
-    
+
     public void loopSound(final String key) {
         playSound(key, true);
     }
-    
+
     public void playSound(final String key) {
         playSound(key, false);
     }
+
     //The caller is obligated to make sure that file that it's trying to play has passed (isFileSupported)
+
     private void playSound(final String key, final boolean loop) {
-        Thread thread = new Thread(new Runnable() {
-            // The wrapper thread is unnecessary, unless it blocks on the
-            // Clip finishing; see comments.
-            @Override
-            public void run() {
-                File clipFile = keyToFile.get(key);
-                class AudioListener implements LineListener {
-
-                    private boolean done = false;
-
-                    @Override
-                    public synchronized void update(LineEvent event) {
-                        LineEvent.Type eventType = event.getType();
-                        if (eventType == LineEvent.Type.STOP || eventType == LineEvent.Type.CLOSE) {
-                            done = true;
-                            notifyAll();
-                        }
-                    }
-
-                    public synchronized void waitUntilDone() throws InterruptedException {
-                        while (!done) {
-                            wait();
-                        }
-                    }
-                }
-                AudioListener listener = new AudioListener();
-                try (AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(clipFile)) {
-                    Clip clip = AudioSystem.getClip();
-                    clip.addLineListener(listener);
-                    clip.open(audioInputStream);
-                    try {
-                        if (loop){
-                            clip.loop(Clip.LOOP_CONTINUOUSLY);
-                        } else {
-                            clip.start();
-                        }
-                        listener.waitUntilDone();
-                    } catch (InterruptedException ex) {
-                        //Ok, we're being stopped;
-                    } finally {
-                        clip.close();
-                    }
-                } catch (LineUnavailableException | UnsupportedAudioFileException | IOException ex) {
-                    Logger.getLogger(SoundTableModel.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }
-        });
-        keyToThreads.get(key).add(thread);
+        SoundRunnable tr = new SoundRunnable(keyToFile.get(key), loop);
+        Thread thread = new Thread(tr);
+        keyToRunnable.get(key).add(tr);
         thread.start();
     }
-    
+
     @Override
     public String getColumnName(int col) {
-        if (col == 0){
+        if (col == 0) {
             return "Keyword";
-        } else if (col == 1){
+        } else if (col == 1) {
             return "File";
         } else {
             return "!!ERROR!!";
         }
     }
+
     @Override
-    public int getRowCount() { return keyToFile.keySet().size(); }
+    public int getRowCount() {
+        return keyToFile.keySet().size();
+    }
+
     @Override
-    public int getColumnCount() { return 2; }
+    public int getColumnCount() {
+        return 2;
+    }
+
     @Override
     public Object getValueAt(int row, int col) {
-        if (col == 0){
+        if (col == 0) {
             return keyToFile.keySet().toArray()[row];
-        } else if (col == 1){
+        } else if (col == 1) {
             return keyToFile.get(keyToFile.keySet().toArray()[row]).getName();
         } else {
             return "!!ERROR!!";
         }
     }
+
     @Override
-    public boolean isCellEditable(int row, int col)
-        { return false; }
-    
+    public boolean isCellEditable(int row, int col) {
+        return false;
+    }
+
 }
